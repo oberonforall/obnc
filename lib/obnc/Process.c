c8aff9b87388a6d496ef4ddc09433ef78e8270ec
amtoine <44101798+amtoine@users.noreply.github.com>
2022-11-28 16:58:29 +0100

    add the Process module to execute external processes
    Related to https://github.com/oberonproject/compiler/issues/103

---
+++ lib/obnc/Process.c
@@ -0,0 +1,46 @@
+#include ".obnc/Process.h"
+#include <obnc/OBNC.h>
+#ifdef _WIN32
+	#include <windows.h>
+#else
+	#include <unistd.h>
+#endif
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+long Process__Execute_(const char* path, OBNC_INTEGER path_len, const char argv[], OBNC_INTEGER argv_len, OBNC_INTEGER argv_len1)
+{
+  int pid, status;
+  long code;
+
+  char* complete_argv[argv_len + 2];
+
+  complete_argv[0] = (char*) path;
+  for (int i = 0; i <= argv_len; i++) {
+    complete_argv[i + 1] = (char*) &argv[i * argv_len1];
+  }
+  complete_argv[argv_len + 1] = NULL;
+
+  switch (pid = fork()) {
+    case 0:
+      code = (long) execv(path, complete_argv);
+      return code;
+      exit(0);
+    default:
+      pid = wait(&status);
+      break;
+  }
+  return -1;
+}
+
+
+void Process__Init(void)
+{
+}
+
