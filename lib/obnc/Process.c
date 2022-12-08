e9b6d3b2f654965d84dffea820c9ca03c024a0ef
amtoine <44101798+amtoine@users.noreply.github.com>
2022-12-08 10:55:35 +0100

    change `Execute` signature and return stdout buffer
    This commit:
    - accept a `VAR` exit code as first argument
    - return a `CHAR` buffer
    - add comments
    - use `*_FILENO` to call `stdout` and `stderr`
    - use `exit(EXIT_*)` to exit properly
    - add the case of `fork() == -1` in the `switch`es
    - use `pipe`, `pfd` and `buffer` to extract `stdout`
    from the child process's pipe

--- lib/obnc/Process.c
+++ lib/obnc/Process.c
@@ -15,47 +15,92 @@
 #include <time.h>
 #include <fcntl.h>
 
-long Process__Execute_(const char* command, OBNC_INTEGER path_len, const char argv[], OBNC_INTEGER argv_len, OBNC_INTEGER argv_len1)
+
+char* execute(long* code, const char* command, const char argv[], long argv_len, long argv_len1)
 {
   int pid, status, fd;
-  long code = 0;
+  int pfd[2];
 
+  // check that the command does exist with `which`
   switch (pid = fork()) {
-    case 0:
+    case -1:
+      perror("fork");
+      exit(EXIT_FAILURE);
+    case 0:  // child
       fd = open("/dev/null", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-      dup2(fd, 1);  // make stdout go null
+      dup2(fd, STDOUT_FILENO);  // make stdout go null
       close(fd);
 
       execlp("which", "which", command, NULL);
-      exit(0);
-    default:
+      exit(EXIT_SUCCESS);
+    default:  // parent
       pid = wait(&status);
-      code = WEXITSTATUS(status);
+      *code = WEXITSTATUS(status);
       break;
   }
 
-  if (code != 0) {
-    return -1;
+  if (*code != 0) {
+    // the command does not exist
+    return "command not found";
   }
 
+  // build the complete set of arguments for `exec`
+  // see `man 3 execv`
   char* complete_argv[argv_len + 2];
-
+  // prepend the name of the command as "$0"
   complete_argv[0] = (char*) command;
+  // add the "real" arguments to the command
   for (int i = 0; i <= argv_len; i++) {
     complete_argv[i + 1] = (char*) &argv[i * argv_len1];
   }
+  // append a NULL pointer to terminate the list of arguments
   complete_argv[argv_len + 1] = NULL;
 
+  // open the pipe to get stdout back
+  if (pipe(pfd) == -1){
+    perror("error");
+    exit(EXIT_FAILURE);
+  }
+
+  char buffer[1000];
+  // run the command
   switch (pid = fork()) {
-    case 0:
+    case -1:
+      perror("fork");
+      exit(EXIT_FAILURE);
+    case 0:  // child
+      close(pfd[0]);  // close unused read end
+      dup2(pfd[1], STDOUT_FILENO);
+      close(pfd[1]);  // close so that the reader will see EOF
+
       execvp(command, complete_argv);
-      exit(0);
-    default:
-      pid = wait(&status);
-      code = WEXITSTATUS(status);
+      exit(EXIT_SUCCESS);
+    default:  // parent
+      close(pfd[1]);  // close unused write end
+
+      long size = read(pfd[0], &buffer, 1000);  // read the pipe
+
+      if ( (size > 0) && (size < sizeof(buffer)) ) {
+      // add a null character to the end of the buffer
+        if (buffer[size - 1] == '\n') {
+          // remove the trailing newline
+          buffer[size - 1] = '\0';
+        } else {
+          buffer[size] = '\0';
+        }
+      } else {
+        perror("read pipe");
+        exit(EXIT_FAILURE);
+      }
+      printf("");  // DO NOT REMOVE OR I WILL CRASH
+      close(pfd[0]);
+
+      pid = wait(&status);  // wait for the child
+      *code = WEXITSTATUS(status);  // get the error code of the child process
       break;
   }
-  return code;
+
+  return buffer;
 }
 
 
